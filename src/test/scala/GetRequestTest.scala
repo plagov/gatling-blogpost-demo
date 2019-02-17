70363e731c1ae3bbdb73c4b1b5a51097e64fb458
Vitalii Plagov <vitalii.plagov@gmail.com>
2019-02-17 22:23:37 +0200

    Finish with the GET request scenario
    This scenario describes the simple GET request that is being sent over
    certain period of time.

--- src/test/scala/GetRequestTest.scala
+++ src/test/scala/GetRequestTest.scala
@@ -1,6 +1,6 @@
 import io.gatling.core.Predef._
-import io.gatling.http.Predef.http
-import io.gatling.http.Predef._
+import io.gatling.http.Predef.{http, _}
+import io.gatling.http.protocol.HttpProtocolBuilder
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -10,20 +10,22 @@ class GetRequestTest extends Simulation {
   private val url = "https://postman-echo.com/get"
   private val queryParams = Map("foo1" -> "bar1", "foo2" -> "bar2")
 
-  private def httpConfBuilder(envUrl: String) = {
+  private def httpConfBuilder(envUrl: String): HttpProtocolBuilder = {
     http
       .baseUrl(envUrl)
       .acceptHeader("application/json")
   }
 
-  private val getDateFromEndpoint = scenario("Get date from endpoint")
+  private val sendGetRequest = scenario("Get date from endpoint")
     .exec(http("GET method")
       .get("/")
       .queryParamMap(queryParams)
       .check(status is 200, responseTimeInMillis lte 500))
 
-  setUp(getDateFromEndpoint.inject(
-    rampUsers(200) during(2 minutes)
-  ))
+  setUp(sendGetRequest.inject(
+    rampUsers(200) during(2 minutes),
+    rampUsers(300) during (2 minutes),
+    rampUsers(200) during(1 minutes)
+  )).protocols(httpConfBuilder(url))
 
 }
