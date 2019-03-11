c9787bee256b2f5878118d3cd5aee8abfb60c256
Vitalii Plagov <vitalii.plagov@gmail.com>
2019-03-11 22:48:46 +0200

    Add test for POST method with feeder

--- src/test/scala/PostRequestTest.scala
+++ src/test/scala/PostRequestTest.scala
@@ -1,11 +1,14 @@
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
-import io.gatling.http.Predef.http
+import io.gatling.http.Predef.{http, _}
 import io.gatling.http.protocol.HttpProtocolBuilder
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class PostRequestTest extends Simulation {
 
-  private val url = "https://postman-echo.com/get"
+  private val url = "https://postman-echo.com/post"
   private val feeder = csv("random_strigs.csv")
 
   private def httpConfBuilder(envUrl: String): HttpProtocolBuilder = {
@@ -15,7 +18,16 @@ class PostRequestTest extends Simulation {
   }
 
   private val sendPostRequest = scenario("Send POST request to endpoint")
-    .exec(http("POST method").post(url))
+    .feed(feeder)
+    .exec(
+      http("POST method")
+        .post(url)
+        .queryParamMap(Map("id" -> "${id}"))
+        .check(status is 200, responseTimeInMillis lte 800))
 
-
+  setUp(sendPostRequest.inject(
+    rampUsers(200) during (2 minutes),
+    rampUsers(300) during (2 minutes),
+    rampUsers(200) during (1 minutes)
+  )).protocols(httpConfBuilder(url))
 }
